950a6ffaac6ec6b5345f1543218965edb3ca3338
Abdoul-lelou <abdourahmane.benzy@gmail.com>
2026-01-19 15:46:51 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Shell } from './components/layout/Shell';
 import { Dashboard } from './pages/Dashboard';
